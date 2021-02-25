06d7882f7e440bc9eee6746e7b223ccda8cd2989
Keith James <dev@deoxybyte.co.uk>
2021-02-25 12:26:38 +0000

    Relocate repository from github.com/kjsanger to github.com/wtsi-npg

--- zlog/log.go
+++ zlog/log.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019, 2020. Genome Research Ltd. All rights reserved.
+ * Copyright (C) 2019, 2020, 2021. Genome Research Ltd. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,9 +25,9 @@ import (
 	"io"
 	"time"
 
-	"github.com/kjsanger/logshim"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
+	"github.com/wtsi-npg/logshim"
 )
 
 func translateLevel(level logshim.Level) (zerolog.Level, error) {
